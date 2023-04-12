960178533981d1adc578860e14199e3ff2c6fbf9
kouyate karim <kouyatekarim02@gmail.com>
2023-04-12 09:15:58 +0000

    correction

--- Exercice3/script.js
+++ Exercice3/script.js
@@ -1,20 +1,37 @@
 
-let showHxContent = document.querySelector('.showHxContent(1)')
-showHxContent.addEventListener('click',showHxContent)
+// let showHxContent = document.querySelector('.showHxContent(1)')
+// showHxContent.addEventListener('click',showHxContent)
 
- showHxContent = document.querySelector('.showHxContent(2)')
-showHxContent.addEventListener('click',showHxContent)
+//  showHxContent = document.querySelector('.showHxContent(2)')
+// showHxContent.addEventListener('click',showHxContent)
+var title = document.querySelector('.title');
+let input = document.querySelector('.input');
+let btnSubmit = document.querySelector('.btn-submit');
 
 
-function  showHxContent(params) {
-    if ( showHxContent == document.querySelector('.showHxContent(1)') ) {
-        let title1 = document.querySelector('.title1')
-        title1.classList.add('titre1')
+
+input.addEventListener('input', changeTitle);
+btnSubmit.addEventListener('click', showTitle);
+
+
+function changeTitle(e) {
+    title.innerHTML = e.target.value;
+}
+
+function showTitle() {
+    alert(title.innerHTML);
+}
+
+
+// function  showHxContent(params) {
+//     if ( showHxContent == document.querySelector('.showHxContent(1)') ) {
+//         let title1 = document.querySelector('.title1')
+//         title1.classList.add('titre1')
         
-    }else{
-        let title2 = document.querySelector('.title2')
-        title2.classList.add('titre2')
-    }
+//     }else{
+//         let title2 = document.querySelector('.title2')
+//         title2.classList.add('titre2')
+//     }
 
     
-}
+// }
